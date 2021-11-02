a98a7b828449d02834a9decaf35ef53538ecd893
lavender <id0421942@gmail.com>
2021-11-02 20:21:30 +0800

    add edit,delete,search

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express')
 const exphbs =  require('express-handlebars')
 const mongoose =require('mongoose')
 const Restaurant = require("./models/Restaurant")
+const bodyParser = require('body-parser')
+const methodOverride = require("method-override")
 
 const app = express()
 const port = 3000
@@ -10,8 +12,11 @@ const restaurantList = require('./restaurant.json')
 
 mongoose.connect('mongodb://localhost/restaurant-curd',{ useNewUrlParser: true, useUnifiedTopology: true })
 
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static("public"))
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set ('view engine', 'handlebars')
+app.use(methodOverride("_method"))
 
 // 取得資料庫連線狀態
 const db = mongoose.connection
@@ -35,13 +40,25 @@ app.get('/', (req, res) => {
 })
 
 // 搜尋特定餐廳
-app.get('/search', (req, res) => {
-  const keyword = req.query.keyword.toLowerCase().trim()
-  const restaurants = restaurantList.results.filter(
-    (restaurant) => restaurant.name.toLowerCase().includes(keyword) || restaurant.category.toLowerCase().includes(keyword)
-  )
-  // { restaurants : restaurants , keyword : keyword } object literal extension
-  res.render('index', { restaurants, keyword })
+app.get("/search", (req, res) => {
+  if (!req.query.keywords) {
+    res.redirect("/")
+  }
+
+  const keywords = req.query.keywords
+  const keyword = req.query.keywords.trim().toLowerCase()
+
+  Restaurant.find({})
+    .lean()
+    .then(restaurantsData => {
+      const filterRestaurantsData = restaurantsData.filter(
+        data =>
+          data.name.toLowerCase().includes(keyword) ||
+          data.category.includes(keyword)
+      )
+      res.render("index", { restaurantsData: filterRestaurantsData, keywords })
+    })
+    .catch(err => console.log(err))
 })
 
 // 新增餐廳頁面
@@ -65,6 +82,31 @@ app.get('/restaurants/:restaurantId', (req, res) => {
     .catch(err => console.log(err))
 })
 
+// 編輯餐廳頁面
+app.get("/restaurants/:restaurantId/edit", (req, res) => {
+  const { restaurantId } = req.params
+  Restaurant.findById(restaurantId)
+    .lean()
+    .then(restaurantData => res.render("edit", { restaurantData }))
+    .catch(err => console.log(err))
+})
+
+// 更新餐廳
+app.put("/restaurants/:restaurantId", (req, res) => {
+  const { restaurantId } = req.params
+  Restaurant.findByIdAndUpdate(restaurantId, req.body)
+    //可依照專案發展方向自定編輯後的動作，這邊是導向到瀏覽特定餐廳頁面
+    .then(() => res.redirect(`/restaurants/${restaurantId}`))
+    .catch(err => console.log(err))
+})
+
+// 刪除餐廳
+app.delete("/restaurants/:restaurantId", (req, res) => {
+  const { restaurantId } = req.params
+  Restaurant.findByIdAndDelete(restaurantId)
+    .then(() => res.redirect("/"))
+    .catch(err => console.log(err))
+})
 
 
 // start and listen on the Express server
