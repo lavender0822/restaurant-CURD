9c197f96886a214f733887de8b36d675859007d9
lavender <id0421942@gmail.com>
2021-11-04 15:36:02 +0800

    search

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require("method-override")
-
 const routes = require('./routes')
 require('./config/mongoose')
 
@@ -11,35 +10,10 @@ const port = 3000
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
-
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static("public"))
 app.use(methodOverride("_method"))
 app.use(routes)
-
-
-// 搜尋特定餐廳
-app.get("/search", (req, res) => {
-  if (!req.query.keywords) {
-    res.redirect("/")
-  }
-
-  const keywords = req.query.keywords
-  const keyword = req.query.keywords.trim().toLowerCase()
-
-  Restaurant.find({})
-    .lean()
-    .then(restaurantsData => {
-      const filterRestaurantsData = restaurantsData.filter(
-        data =>
-          data.name.toLowerCase().includes(keyword) ||
-          data.category.includes(keyword)
-      )
-      res.render("index", { restaurantsData: filterRestaurantsData, keywords })
-    })
-    .catch(err => console.log(err))
-})
-
 
 // start and listen on the Express server
 app.listen(port, () => {
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,14 +1,35 @@
 const express = require('express')
 const router = express.Router()
-
 const Restaurant = require('../../models/Restaurant')
 
 // 瀏覽全部餐廳
 router.get('/', (req, res) => {
-    Restaurant.find({})
+  Restaurant.find({})
     .lean()
     .then(restaurantsData => res.render('index', { restaurantsData }))
-    .catch(error => console.error(error))  
-  })
+    .catch(error => console.error(error))
+})
 
-  module.exports = router
+// 搜尋特定餐廳
+router.get("/search", (req, res) => {
+  if (!req.query.keywords) {
+    res.redirect("/")
+  }
+
+  const keywords = req.query.keywords
+  const keyword = req.query.keywords.trim().toLowerCase()
+
+  Restaurant.find({})
+    .lean()
+    .then(restaurantsData => {
+      const filterRestaurantsData = restaurantsData.filter(
+        data =>
+          data.name.toLowerCase().includes(keyword) ||
+          data.category.includes(keyword)
+      )
+      res.render("index", { restaurantsData: filterRestaurantsData, keywords })
+    })
+    .catch(err => console.log(err))
+})
+
+module.exports = router
