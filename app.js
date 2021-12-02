60ebdb368728732b9d367fc05f9bb5a418cdb970
lavender <id0421942@gmail.com>
2021-12-02 20:48:49 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // 項目資料
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const methodOverride = require("method-override")
 const routes = require('./routes')
@@ -10,6 +11,12 @@ const port = 3000
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static("public"))
 app.use(methodOverride("_method"))
